baaa9e5defc678ff0d37affe4b3a7fe32a19d88a
Halbard <HalbardHobby@users.noreply.github.com>
2021-08-31 18:29:03 -0500

    🖊 Implemented current user middleware

--- auth/middleware/middleware.go
+++ auth/middleware/middleware.go
@@ -2,13 +2,37 @@ package middleware
 
 import (
 	"bytes"
+	"context"
 	"io/ioutil"
 	"net/http"
+	"os"
 
 	"github.com/HalbardHobby/TicketingAppMicroservices/auth/data"
 	"github.com/HalbardHobby/TicketingAppMicroservices/auth/errors"
 	"github.com/go-playground/validator/v10"
+	"github.com/golang-jwt/jwt"
 )
+
+const UserContextKey = "User"
+
+func GetCurrentUser(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
+		tokenCookie, err := r.Cookie("session")
+		if err != nil {
+			next.ServeHTTP(rw, r)
+		}
+
+		payload, err := jwt.ParseWithClaims(tokenCookie.Value, new(data.User), func(t *jwt.Token) (interface{}, error) {
+			return []byte(os.Getenv("JWT_KEY")), nil
+		})
+		if err != nil {
+			next.ServeHTTP(rw, r)
+		}
+
+		ctx := context.WithValue(r.Context(), UserContextKey, payload.Claims)
+		next.ServeHTTP(rw, r.WithContext(ctx))
+	})
+}
 
 func ValidateUserInput(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
