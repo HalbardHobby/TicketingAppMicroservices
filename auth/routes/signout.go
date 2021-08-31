db0fd92fe1864074dca1a606f6bfbff9b2951148
Halbard <HalbardHobby@users.noreply.github.com>
2021-08-30 21:42:21 -0500

    👍 Implemented signout

--- auth/routes/signout.go
+++ auth/routes/signout.go
@@ -1,10 +1,21 @@
 package routes
 
 import (
-	"fmt"
+	"encoding/json"
 	"net/http"
 )
 
 func SignOut(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, there")
+	cookie := &http.Cookie{
+		Name:   "session",
+		MaxAge: -1,
+		Value:  "",
+		Path:   "/",
+	}
+
+	http.SetCookie(w, cookie)
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+	w.Header().Set("X-Content-Type-Options", "nosniff")
+	w.WriteHeader(http.StatusNoContent)
+	json.NewEncoder(w).Encode(map[string]string{})
 }
